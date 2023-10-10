7e1da95668ea570cdd2ee68de05e455e0ab5caaa
brunogrossi4 <105368343+brunogrossi4@users.noreply.github.com>
2023-10-10 11:15:53 -0300

    gg

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('.tecla_pom').onclick = tocasomPom;
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 function tocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
 }
-document.querySelector('.tecla_clap').onclick = tocasomClap;
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
 function tocaSomTim(){
     document.querySelector('#som_tecla_tim').play();
 }
-document.querySelector('.tecla_tim').onclick = tocasomTim;
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
