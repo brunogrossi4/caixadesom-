21f545b75e60fa43769f5fe57b13b9be9920f1a8
brunogrossi4 <105368343+brunogrossi4@users.noreply.github.com>
2023-10-24 11:31:24 -0300

    koikfod

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ let contador = 0;
 while(contador < listaDeTeclas.length){
     const efeito = listaDeTeclas[contador].classList[1];
     const idElementoAudio = "#som_"+efeito;
-     listaDeTeclas[contador].onclick = function();{
+     listaDeTeclas[contador].onclick = function(){
         tocaSom (idElementoAudio);
      }
      contador = contador + 1;
