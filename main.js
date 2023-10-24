a38346cc53fda67fa8127add22b165536cc41f03
brunogrossi4 <105368343+brunogrossi4@users.noreply.github.com>
2023-10-24 11:27:59 -0300

    aaaaaaaaaaaaa

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 //chamando/invocando a funçao tocaSomPom pelo clique do botão pom
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 
 //criando referência constante ListaDeTeclas buscando todos
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //invocando a função tocaSomPom a partir do item 0, 1º botão
-listaDeTeclas[0].onclick = tocaSomPom;
 
 //criando referência variável "contador" iniciando = 0
 let contador = 0;
