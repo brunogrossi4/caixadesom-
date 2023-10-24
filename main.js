7c4420f240203a4b2985f2eb1cfd3943a1810924
brunogrossi4 <105368343+brunogrossi4@users.noreply.github.com>
2023-10-24 11:27:06 -0300

    ggggggg

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 //chamando/invocando a funçao tocaSomPom pelo clique do botão pom
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 
 //criando referência constante ListaDeTeclas buscando todos
